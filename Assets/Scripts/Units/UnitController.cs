b6a835d84e78ea35f6cf53f77216eb4c0cb7d30b
Eray <77186648+Eraycak@users.noreply.github.com>
2023-04-08 19:40:31 +0300

    Mouse drag is done
    Player can drag units with a mouse.

--- Assets/Scripts/Units/UnitController.cs
+++ Assets/Scripts/Units/UnitController.cs
@@ -1,10 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UnitController : MonoBehaviour
 {
     private Outline outline;
+    private Vector3 worldPosition;
+    private Plane plane = new Plane(Vector3.up, 0);
+    //private bool isCollided = false;
+    void Update()
+    {
+        float distance;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out distance))
+        {
+            worldPosition = ray.GetPoint(distance);
+        }
+    }
 
     private void OnMouseEnter()
     {
@@ -23,6 +37,33 @@ public class UnitController : MonoBehaviour
 
     private void OnMouseExit()
     {
+        /*if (isCollided)
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(2, 0, 0);
+        }*/
         gameObject.GetComponent<Outline>().enabled = false;
     }
+
+    private void OnMouseDrag()
+    {
+        gameObject.transform.position = new Vector3(worldPosition.x, 1, worldPosition.z);
+    }
+
+    private void OnMouseUp()
+    {
+        gameObject.transform.position = new Vector3(worldPosition.x, 0, worldPosition.z);
+    }
+
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name != "Plane")
+        {
+            isCollided = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isCollided = false;
+    }*/
 }
